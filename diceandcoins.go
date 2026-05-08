dcaf05fb1a921beb4b4905f0d4c8beee241af079
Nathan Doolan <nathan.doolan@kaluza.com>
2026-05-09 00:01:30 +1000

    Add go package with embedded static files

---
+++ diceandcoins.go
@@ -0,0 +1,10 @@
+package diceandcoins
+
+import (
+	"embed"
+)
+
+//go:embed index.html
+//go:embed src
+//go:embed styles
+var FS embed.FS
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go.doolan.dev/tools/dice-and-coins
+
+go 1.25.1
